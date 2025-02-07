1bd4dd458aee5de699a887152197a345044f1f26
hkamori <hk1337@proton.me>
2025-02-07 19:21:31 +0100

    Update stop.py

--- modules/stop.py
+++ modules/stop.py
@@ -5,7 +5,7 @@ commands = ["stop", "exit"]
 
 async def handle(app: Client, client: Client, message, args):
     global stop_toggle
-    me = config.read_from_config('mainemoji')
+    me = config.read('mainemoji')
     stop_toggle = True
     await app.send_message(message.chat.id, f"{me} `kaleidoscope` is now closed.`")
     exit()
