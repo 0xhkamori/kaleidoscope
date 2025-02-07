5149e9842978c438f1c5bd9358c65980e04094b4
hkamori <hk1337@proton.me>
2025-02-07 19:11:03 +0100

    Update setprefix.py

--- modules/setprefix.py
+++ modules/setprefix.py
@@ -4,8 +4,7 @@ from utils import config
 commands = ["setprefix"]
 
 async def handle(app: Client, client: Client, message, args):
-    me = config.read_from_config('mainemoji')
-    new_prefix = " ".join(args)  # Assuming args is a list of words for the new prefix
-    # Update the global prefix variable (this might need to be passed differently)
+    me = config.read('mainemoji')
+    new_prefix = " ".join(args)
     config.add_to_config('prefix', new_prefix)
     await app.send_message(message.chat.id, f"{me} **Prefix changed.** \n💫 New prefix: `({new_prefix})`")
