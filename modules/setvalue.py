f88e99f35a7c9ea9b03cf294ed180e35c1002e29
hkamori <hk1337@proton.me>
2025-02-07 19:11:25 +0100

    Update setvalue.py

--- modules/setvalue.py
+++ modules/setvalue.py
@@ -7,7 +7,7 @@ async def handle(app: Client, client: Client, message, args):
     try:
         key = args[0]
         value = args[1]
-        config.add_to_config(key, value)
+        config.add(key, value)
         await app.send_message(message.chat.id, "✅  **Value set successfully**")
     except:
         await app.send_message(message.chat.id, "🛑  **The command is not used correctly**")
